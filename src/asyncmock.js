40f055cbdb1e1f3a604b739d4aa95a3c9ebf8605
Emmanuel Cortese <emmanuelcortese182@gmail.com>
2024-01-25 00:18:14 -0300

    preentrega2reactcortese

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,8 +1,8 @@
 const productos= [
-    {id: 1, nombre: "Producto1", precio: 100, img: "./img/Carrito.png", idCat: "1"},
-    {id: 2, nombre: "Producto2", precio: 200, img: "./img/Carrito.png", idCat: "2"},
-    {id: 3, nombre: "Producto3", precio: 300, img: "./img/Carrito.png", idCat: "1"},
-    {id: 4, nombre: "Producto4", precio: 400, img: "./img/Carrito.png", idCat: "2"},
+    {id: 1, nombre: "Producto1", precio: 100, img: "./img/BlackClover.webp", idCat: "1"},
+    {id: 2, nombre: "Producto2", precio: 200, img: "./img/BlueLock.webp", idCat: "2"},
+    {id: 3, nombre: "Producto3", precio: 300, img: "./img/CSM.webp", idCat: "1"},
+    {id: 4, nombre: "Producto4", precio: 400, img: "./img/JK.webp", idCat: "2"},
 ]
 
 export const getProductos = () => {
